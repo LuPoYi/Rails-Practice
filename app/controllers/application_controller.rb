ddceaccdb709ab6690e078696e5229fffcb97b5a
LuPoYI <maydaybob2000@gmail.com>
2013-12-28 11:24:38 +0800

    fix devise name problem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
   def login_required
@@ -19,6 +20,15 @@ class ApplicationController < ActionController::Base
 	end
 	
   end
+
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    # important line for me
+  end
+
 
 
 
